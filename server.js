db337dd8f91afbdfb3b1c32e04f24fb6f582f4f3
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-09-16 10:07:27 -0400

    took the code that I used to make activity 17 to work and pluged it in to see if it will do the same thing

--- server.js
+++ server.js
@@ -7,7 +7,40 @@ const app = express();
 
 app.use(express.static('public'));
 
-// GET Route for homepage
+app.get('/api/reviews', (req, res) => {
+  res.status(200).json(reviews);
+});
+
+
+app.post('/api/reviews', (req, res) => {
+  
+  console.info(`${req.method} request received to add a review`);
+
+  
+  const { product, review, username } = req.body;
+
+  
+  if (product && review && username) {
+   
+    const newReview = {
+      product,
+      review,
+      username,
+      review_id: uuid(),
+    };
+
+    const response = {
+      status: 'success',
+      body: newReview,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
+
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
