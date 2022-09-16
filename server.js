323aa8db8052644df8eccd418ef39978113ef4f0
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-09-15 21:54:47 -0400

    added notes to my server

--- server.js
+++ server.js
@@ -12,4 +12,8 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
