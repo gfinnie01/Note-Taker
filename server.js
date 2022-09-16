ddfa2d6d5eaa875ad3daccbd8077bd0f5608faa7
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-09-16 10:27:32 -0400

    took away the constants for html

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
-const review = review.json;
+
 const PORT = process.env.PORT || 3001;
-const index = index.html;
+
 
 const app = express();
 
