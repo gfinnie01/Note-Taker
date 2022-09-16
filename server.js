5bbd57eca9850604949209df15a308526a3aa98f
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-09-16 11:48:48 -0400

    just fixed all my errors repathed to make notes api work also got my id to work and made my second json file

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const path = require('path');
 const uuid = require('./helpers/uuid');
 const review = require('./db/review');
+const fs = require('fs')
+const util = require('util')
 
 const PORT = process.env.PORT || 3001;
 
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -23,8 +27,7 @@ app.post('/api/notes', (req, res) => {
   const { title, text } = req.body;
 
   
-  if (title && text) {
-   
+  if (req.body) {
     const newReview = {
      title,
      text,
@@ -50,6 +53,9 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/jnotes.json').then((data) => res.json(JSON.parse(data)));
+});
+
 app.listen(PORT, () => console.log(`App listening on port http://localhost:${PORT}`));
-
-
